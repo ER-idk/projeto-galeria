e3b5f9bb7fce75220221a4d9d133d95b17682a0b
Elias Rafael <78946129+ER-idk@users.noreply.github.com>
2022-02-01 13:46:30 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Arquivo SASS
+import './scss/index.scss';
+
+// Dependencias
+import 'jquery';
+import 'bootstrap';
+
+// Meus arquivos JS
+import './js/main/includes';
+import './js/plugins/cityButtons';
---
+++ src/js/main/includes.js
@@ -0,0 +1,29 @@
+import $ from 'jquery';
+
+const loadHtmlSuccessCalbacks = [];
+
+export function onLoadHtmlSuccess(callback) {
+    if(!loadHtmlSuccessCalbacks.includes(callback)) {
+        loadHtmlSuccessCalbacks.push(callback);
+    }
+}
+
+function loadIncludes(parent) {
+    if(!parent) parent = 'body';
+    $(parent).find('[wm-include]').each(function(i, e) {
+        const url = $(e).attr('wm-include');
+        $.ajax({
+            url,
+            success(data) {
+                $(e).html(data);
+                $(e).removeAttr('wm-include');
+
+                loadHtmlSuccessCalbacks.forEach(callback => callback(data));
+
+                loadIncludes(e);
+            }
+        })
+    })
+}
+
+loadIncludes();
